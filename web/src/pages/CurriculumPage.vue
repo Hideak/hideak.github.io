65d658ebf60025c4bfdaec0ee65d9cc430745307
Willian Hideak <whideak@gmail.com>
2020-11-22 17:05:43 -0300

    Updated curriculum page

--- web/src/pages/CurriculumPage.vue
+++ web/src/pages/CurriculumPage.vue
@@ -1,7 +1,204 @@
 <template>
   <div>
     <h1>Currículo</h1>
-    <p>Página em construção</p>
+    <p>
+      <em>WILLIAN HIDEAK ARITA DA SILVA</em> <br />
+      <b>Data de nascimento: </b>25/10/1996 - 24 anos <br />
+      <b>Endereço: </b>Rua Antônio Soster, nº 913, Jardim Regina, Indaiatuba - SP <br />
+      <b>E-mail: </b><a rel="noreferrer" href="mailto:whideak@gmail.com">whideak@gmail.com</a>
+      <br />
+      <b>Celular: </b><a href="https://wa.me/5519994115113">+55 (19) 99411-5113</a> <br />
+    </p>
+    <p>
+      <a rel="noreferrer" href="http://lattes.cnpq.br/1071757822049421">Currículo Lattes</a> |
+      <a rel="noreferrer" href="https://hideak.com">Site Pessoal</a> |
+      <a rel="noreferrer" href="https://github.com/hideak">Perfil no Github</a>
+    </p>
+
+    <h2>Objetivos</h2>
+    <p>
+      <em>WILLIAN HIDEAK ARITA DA SILVA</em> é um desenvolvedor de software apaixonado por
+      tecnologia e engenharia. Possui 2 anos e meio de experiência em desenvolvimento de projetos
+      usando metodologias ágeis aplicadas na construção de serviços, aplicações e soluções
+      computacionais. Atualmente, busca ampliar ainda mais seu campo de atuação e conhecimento na
+      área.
+    </p>
+
+    <h2>Histórico Educacional</h2>
+    <p><em>GRADUAÇÃO</em> [MAR/2015 até HOJE] – Universidade Estadual de Campinas (Unicamp).</p>
+    <p>
+      Bacharelado em Engenharia de Controle e Automação. Previsão de conclusão em DEZ/2020.
+    </p>
+
+    <h2>Experiência Profissional</h2>
+
+    <p>
+      <em>ANALISTA DE SOFTWARE</em> [SET/2020 até HOJE] – Instituto de Pesquisas Eldorado –
+      Campinas/SP.
+    </p>
+    <p>
+      Desenvolvimento de aplicações web, aplicações desktop nativas e sistemas SaaS utilizando
+      computação em nuvem.
+    </p>
+
+    <p>
+      <em>ESTAGIÁRIO EM DESENVOLVIMENTO</em> [ABR/2019 até AGO/2020] – Instituto de Pesquisas
+      Eldorado – Campinas/SP.
+    </p>
+    <p>
+      Desenvolvimento de soluções de software envolvendo Aprendizagem de Máquina e Inteligência
+      Artificial.
+    </p>
+
+    <h2>Projetos Acadêmicos em Univerdidades e Centros de Pesquisa</h2>
+
+    <p>
+      <em>INICIAÇÃO CIENTÍFICA (PIC)</em> [AGO/2017 até AGO/2018] – Faculdade de Engenharia Mecânica
+      (FEM).
+    </p>
+
+    <p>
+      Projeto: Desenvolvimento de algoritmo envolvendo redes neurais artificiais (RNAs) para análise
+      de sinais de miografia de força e caracterização de movimentos da mão humana.
+    </p>
+
+    <p>
+      <em>INICIAÇÃO CIENTÍFICA (PIC)</em> [FEV/2016 até JUL/2017] – Centro Nacional de Pesquisa em
+      Energia e Materiais (CNPEM).
+    </p>
+
+    <p>
+      Projeto: Desenvolvimento de software para a geração de nuvens de pontos obtidas por amostragem
+      de modelos CAD. O software encontra-se disponível na forma de código aberto sob a licença do
+      GNU e hospedado na plataforma GitHub.
+    </p>
+
+    <h2>Conhecimentos em Computação</h2>
+
+    <p>
+      <em>DESENVOLVIMENTO FRONT-END</em> – Conhecimento de base em HTML e CSS, utilizados em
+      conjunto com as linguagens de programação Javascript/Typescript aplicadas ao desenvolvimento
+      de aplicações com Angular 10 e VueJS 2, além de domínio parcial sobre Bootstrap 4, Material
+      Design e aplicações do tipo Software as a Service (SaaS).
+    </p>
+
+    <p>
+      <em>DESENVOLVIMENTO BACKEND</em> – Conhecimento em programação orientada a objetos na
+      linguagem Python, Java e C# para desenvolvimento de microsserviços e servidores baseados em
+      Flask e ASP.NET, além de integração com banco de dados MySQL, SQLServer, H2 e Redis.
+    </p>
+
+    <p>
+      <em>DESENVOLVIMENTO DE APLICAÇÕES DESKTOP NATIVAS</em> – Conhecimento sobre aplicações nativas
+      para o sistema Windows utilizando o padrão Model-View-ViewModel (MVVM) junto com Windows
+      Presentation Foundation (WPF).
+    </p>
+
+    <p>
+      <em>APRENDIZADO DE MÁQUINA E INTELIGÊNCIA ARTIFICIAL</em> – Conhecimentos sobre redes neurais
+      artificiais (RNAs), processamento de linguagem natural (PLN) e conhecimento nas bibliotecas
+      Keras e TensorFlow.
+    </p>
+
+    <p>
+      <em>DATA SCIENCE E ANÁLISE DE DADOS</em> – Conhecimentos na linguagem de script do MATLAB e
+      das bibliotecas Numpy e Scipy (análise de dados), além das bibliotecas Pandas, MatPlotLib e
+      Jupyter (ferramentas de visualização).
+    </p>
+
+    <p>
+      <em>VISÃO COMPUTACIONAL E 3D</em> – Conhecimentos sobre a biblioteca OpenCV (processamento de
+      imagens em C++ e Python), OpenCascade (Computer Aided Design – CAD) e ThreeJS (renderização 3D
+      em navegadores via WebGL).
+    </p>
+
+    <p>
+      <em>CONTEINERIZAÇÃO E CD/CI</em> – Conhecimentos sobre Docker, Docker-Compose e Pipelines do
+      Github.
+    </p>
+
+    <p>
+      <em>PACOTE OFFICE</em> – Word, Excel, PowerPoint e desenvolvimento de banco de dados simples
+      com Microsoft Access, atualmente implementadas em 2 lojas agropecuárias na cidade de
+      Indaiatuba.
+    </p>
+
+    <h2>Prêmios e Classificações</h2>
+
+    <p>
+      <em>4 MENÇÕES HONROSAS E 2 MEDALHAS DE BRONZE</em> – [2007, 2008, 2009, 2010, 2012 e 2013]
+      Participação na III, IV, V, VI, VIII e IX edições da Olimpíada Brasileira de Matemática das
+      Escolas Públicas (OBMEP).
+    </p>
+
+    <p>
+      <em>DESAFIO DE INOVAÇÃO TECNOLÓGICA</em> [2018] – Participação no DESAFIO UNICAMP 2018 de
+      Inovação Tecnológica, cuja equipe foi classificada entre as 20 melhores colocadas no Desafio.
+    </p>
+
+    <p>
+      <em>COEFICIENTE DE RENDIMENTO DA UNICAMP (CR)</em> [2017] – 4º MELHOR COLOCADO entre os alunos
+      da turma de 2015 de Engenharia de Controle e Automação em um total de 55 alunos.
+    </p>
+
+    <h2>Cursos e Certificações</h2>
+
+    <p>
+      <em>CURSO DESENVOLVIMENTO MOBILE (ANDROID)</em> [JAN/2020] – Curso de desenvolvimento
+      utilizando Android Studio, sendo realizado através da Udemy Cursos Online.
+    </p>
+
+    <p>
+      <em>CURSO DE DESENVOLVIMENTO WEB COM JAVASCRIPT FUNCIONAL E REATIVO</em> [ABR/2020] – Curso
+      completo de desenvolvimento web oferecido pela Cod3r Cursos Online pelo Prof. Leonardo Leitão.
+    </p>
+
+    <p>
+      <em>PROGRAMA DE INICIAÇÃO CIENTÍFICA JR. (PIC Jr.)</em> [2013 a 2014] – Instituto de
+      Matemática, Estatística e Computação Científica (IMECC). Atividades Realizadas: Aulas de
+      aperfeiçoamento em matemática pura e aplicada.
+    </p>
+
+    <h2>Idiomas</h2>
+
+    <p>
+      <em>PORTUGUÊS FLUENTE</em> (língua nativa) e <em>INGLÊS AVANÇADO</em> (Writing: B2; Listening:
+      B2). Avaliado pela prova do MET (Michigan English Test), baseado no CEFR (Common European
+      Framework of Reference).
+    </p>
+
+    <h2>Publicações Científicas</h2>
+
+    <p>
+      <a rel="noreferrer" href="https://ieeexplore.ieee.org/document/8834067"
+        >[DOI 10.1109/ICARM.2019.8834067]</a
+      >
+      <em> A Wearable Robotic Glove based on Optical FMG driven Controller, 2019</em>
+    </p>
+
+    <p>
+      <a rel="noreferrer" href="https://ieeexplore.ieee.org/document/8797788"
+        >[DOI 10.1109/VR.2019.8797788]</a
+      >
+      <em> Haptic Interface Based on Optical Fiber Force Myography Sensor, 2019</em>
+    </p>
+
+    <p>
+      <a rel="noreferrer" href="https://journals.sagepub.com/doi/10.1177/1179597220912825"
+        >[DOI 10.1177/1179597220912825]</a
+      >
+      <em>
+        Integrated Optical Fiber Force Myography Sensor as Pervasive Predictor of Hand Postures,
+        2020</em
+      >
+    </p>
+
+    <h2>Eventos</h2>
+
+    <p>
+      <em>CONGRESSO DE INICIAÇÃO CIENTÍFICA</em> [2017 e 2018] – Participação no XV e XVI Congresso
+      Interno de Iniciação Científica do PIBIC/CNPEM, com apresentação de trabalho em pôster.
+    </p>
   </div>
 </template>
 
@@ -9,3 +206,10 @@
 import { defineComponent } from 'vue';
 export default defineComponent({ name: 'CurriculumPage' });
 </script>
+
+<style lang="scss" scoped>
+em {
+  font-style: normal;
+  font-weight: bold;
+}
+</style>
